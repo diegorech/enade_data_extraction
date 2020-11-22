ed9500dbd62e4ee91435f5cb87d5713b88a8b383
Diego Rech <diegorech@gmail.com>
2020-11-22 13:25:33 -0300

    Script setup

---
+++ aula_extração.py
@@ -0,0 +1,7 @@
+#  Extração de dados do ENADE utilizando Python
+
+import pandas as pandas
+import numpy as numpy
+import zipfile
+import requests
+from io import BytesIO # Configurar os inputs dos dados
