04e4c3d678c628b0618105184be510c0b3cade58
Diego Rech <diegorech@gmail.com>
2020-11-22 14:23:09 -0300

    extraction complete

--- aula_extração.py
+++ aula_extração.py
@@ -1,7 +1,6 @@
 #  Extração de dados do ENADE utilizando Python
 
 import pandas as pandas
-import numpy as numpy
 import zipfile
 import requests # Realizar request para o site do inep e realizar o download dos dados
 from io import BytesIO # Configurar os inputs dos dados para trabalhar no ambiente python
@@ -28,3 +27,20 @@ filebytes = BytesIO( # Possuí o retorno do request realizado
 myzip = zipfile.ZipFile(filebytes)
 # Extração dos dados
 myzip.extractall('./enade2019')
+
+
+# Gerando um pandas dataframe a partir dos dados baixados
+
+enade_df = pandas.read_csv(
+    './enade2019/microdados_enade_2019/2019/3.DADOS/microdados_enade_2019.txt',
+    sep = ';',
+    decimal = ','
+)
+
+
+# Printa informações sobre o dataframe
+enade_df.info()
+
+# Como o número de colunas é muito grande, antes de printar o tipo de dados de cada uma
+# iremos adiciona-las a um dicionário 
+print(dict(enade_df.dtypes)) 
